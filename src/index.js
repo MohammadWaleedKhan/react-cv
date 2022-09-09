022aa3bbc578630bbf6a6e04efd506dd9b588c40
syedhaashir <hashir.hali@gmail.com>
2022-09-09 22:42:09 +0500

    react-CV

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hi() {
+  return <div>
+          <h1> Mohammad Waleed Khan </h1>
+<address>
+    <ul>
+        <li>House # A-45 Sector 4-A Surjani Town</li>
+        <li>Email Address – smartwaleed@gmail.com</li>
+        <li>Phone # 0348-2234021</li>
+    </ul>
+</address>
+
+    <h3> <cite> Web Developer </cite> </h3>
+
+    <h4>Career Objective: </h4> 
+    <p> To obtain a challenging position where each day brings something new and challenging that desires creativity to solve it, which will allow me to expand upon my education and continue to accumulate knowledge, skills for the, mutual benefit of the organization and my career.
+    </p>
+    <h4>Summary: </h4>
+    <p> Dependable and diligent Web Development who manages multiple tasks and works well under pressure. Experience in installing, troubleshooting and repairing hardware, software and peripherals, plus training of end users.</p>
+        
+    <p>Applies consultative approach to resolving issues. Very patient. Provides clear and simple explanations and works closely with people to develop solutions to their computer problems. Seeking position that requires creating systems and processes to improve quality, productivity and the company's bottom line.
+    </p> 
+    
+    <h3>Development  Skills:</h3>
+    <h4> Web Front-end Development Skills</h4>
+    <p> HTML5, CSS3, Bootstrap, Grid Based Layout Development, Responsive Templates with Media Queries. </p>
+        
+    <h4>  Server Side and Client Side Programming Skills (Basic) </h4>
+    <p> Java Script, jQuery, AJAX basic, PHP Core, MYSQL, CodeIgniter MVC Framework, LARAVEL Framework, Wordpress.</p>
+        
+
+    <h3>Areas of Expertise: </h3>
+        <ul>
+            <li>Mediation and problem solving skills.</li>
+            <li>Work well as a team member or independently.</li>
+            <li>Met or exceeded all performance goals to date. Resourceful, energetic, competent, multi-task and results-oriented.</li>
+        </ul>
+
+       <h4> Programming Experience (Basic):</h4>
+       <ul>
+        <li> Programming of BulkSMS, website by using, PHP Core Programming.</li>
+        <li> Programming of BulkSMS with CMS Panel, by using, PHP, CodeIgniter, jQuery, AJAX.</li>
+        <li> Programming of Mega Laptop, by using PHP, CodeIgniter, jQuery, AJAX.</li>
+        <li> Programming of Mega Books Project with CMS Panel, by using LARAVEL, jQuery, AJAX, JSON, API.</li>
+      </ul>
+        <h4>Experience: </h4>
+        <ul>
+            <li>Fresh</li>
+        </ul>
+        
+       <h3> Education:</h3>
+        <h4> Intermediate (2018)</h4>
+        <p> Govt Degree Boys College, Karachi </p>
+
+        <h4> Matric (2016)</h4>
+        <p> <abbr title="Rose Petal School"></abbr> , Karachi </p>
+
+        <h4> Certifications: </h4>
+            <ul>
+                <li> Web Development Certified - Al-Fateem Academy® Karachi.</li>
+                <li> PHP Programming Certified - Al-Fateem Academy® Karachi</li>
+                <li>WordPress Certified - Al-Fateem Academy® Karachi</li>
+            </ul>    
+        
+            <h4> Personal Profile: </h4>
+                <ul>
+                <li>Full Name		Mohammad Waleed Khan </li>
+                <li>Father Name 		Mohammad Zahid Khan </li>
+                <li>Date Of Birth		05-07-2000 </li>
+                <li>Marital Status		Single </li>
+                <li>Religion		Islam </li>
+                <li>Nationality		Pakistani </li>
+                <li>City			Karachi, Pakistan </li>
+            </ul>
+  </div>;
+}
+
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
